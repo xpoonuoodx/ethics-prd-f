625a61e8765d92f8b58ca6c280b6cd32c941a165
DevOp08 <develop.sp08@gmail.com>
2026-04-09 11:58:03 +0700

    fix: merge 9/4/2026

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./page/Home"; // นำเข้า Home (ปรับ Path ตามโฟลเดอร์ของคุณ)
-
-// จำลอง Component สำหรับหน้าอื่นๆ (สร้างไฟล์แยกได้เลยครับ) v.1
-const Services = () => (
-  <div>
-    <h1>บริการของเรา</h1>
-  </div>
-);
-const Workflow = () => (
-  <div>
-    <h1>ขั้นตอนการทำงาน</h1>
-  </div>
-);
-const Login = () => (
-  <div>
-    <h1>เข้าสู่ระบบ</h1>
-  </div>
-);
-const Register = () => (
-  <div>
-    <h1>สมัครสมาชิก</h1>
-  </div>
-);
+import Home from "./page/Home";
+import Dashboard from "./page/Dashboard";
+// นำเข้าหน้าเกี่ยวกับเราทั้ง 4 หน้า
+import AboutBackground from "./page/AboutBackground";
+import AboutPrinciples from "./page/AboutPrinciples";
+import AboutProcess from "./page/AboutProcess";
+import AboutParticipation from "./page/AboutParticipation";
+import Contact from "./page/Contact";
 
 function App() {
   return (
